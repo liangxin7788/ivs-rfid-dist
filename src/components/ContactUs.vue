f02cd6fa4474bb4f8e927cec25953d960471c1bb
liangxin7788 <lx_4509@163.com>
2020-01-16 11:56:13 +0800

    留言

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -10,9 +10,30 @@
     <br>
     <br>
     <br>
-    <div class="main">
-      <div class="a" v-for="item in introduction.split('\n')">{{item}}</div>
-      <div class="b">右边暂缺</div>
+    <div class="con">
+      <div class="a">
+        <div v-for="item in introduction.split('\n')">{{item}}</div>
+      </div>
+      <div class="b">
+        <el-form label-width="80px">
+          <div class="c">
+              <el-input type="input" v-model="name" placeholder="Your Name(required)" size="medium"></el-input>
+              <el-input type="input" v-model="customerEmail" placeholder="E-mail(required)" size="medium"></el-input>
+          </div><br>
+          <div class="c">
+              <el-input type="input" v-model="comeFrom" placeholder="Country(un-required)" size="medium"></el-input>
+              <el-input type="input" v-model="company" placeholder="Company(un-required)" size="medium"></el-input>
+              <el-input type="input" v-model="phoneNumber" placeholder="Phone(un-required)" size="medium"></el-input>
+          </div>
+          <el-form-item label="Massage:">
+            <el-input type="textarea" :autosize="{ minRows: 8, maxRows: 50}" v-model="massage" style="margin: 50px 0; width: 500px; text-align: left"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">Submit</el-button>
+            <el-button @click="resetForm">Cancel</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
@@ -27,7 +48,13 @@
             {url:require('../assets/Long-image-3.jpg')},
             {url:require('../assets/Long-image-4.jpg')}
           ],
-          introduction: ''
+          introduction: '',
+          name: '',
+          customerEmail: '',
+          phoneNumber: '',
+          comeFrom: '',
+          company: '',
+          massage: ''
         }
       },
       mounted() {
@@ -35,23 +62,67 @@
           'Address: 深圳市龙岗区平湖街道新木社区新木路321-3号新木半里大厦C座1413\n' +
           'Tel: (0755)8888 8888\nFax: (0755)8888 8889\nP.C: 351100\nE-mail: Sharon.loo@outlook.com\n' +
           'WhatsApp: +86 158 1429 5175'
+      },
+      methods: {
+        onSubmit() {
+          console.log('submit!');
+          req.postRequest('/customerMassage/addMassage',{
+            name: this.name || undefined,
+            customerEmail: this.customerEmail || undefined,
+            massage: this.massage || undefined,
+            phoneNumber: this.phoneNumber || undefined,
+            comeFrom: this.comeFrom || undefined,
+            company: this.company || undefined,
+          }).then(res => {
+            console.log(res.data.result);
+          }).catch(e => {
+            console.log(e);
+          })
+        },
+        resetForm() {
+          return {
+            name: '',
+            customerEmail: '',
+            massage: '',
+            phoneNumber: '',
+            comeFrom: '',
+            company: ''
+          }
+        }
       }
     }
 </script>
 
 <style >
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  .con {
+    /*width: 500px;*/
+    height: 400px;
+    margin: 0 200px;
+  }
+  .con > div {
+    width: 50%;
+    /*height: 100%;*/
+  }
   .a {
     text-align: left;
-    line-height: 30px;
-    margin-left: 500px;
-    width: 350px;
+    line-height: 40px;
+    margin-left: 0;
+    width: 400px;
     /*background: red;*/
-    /*float: left;*/
+    float: left;
   }
-  /*.b {*/
-  /*  background: yellow;*/
-  /*  float: right;*/
-  /*}*/
+  .c .el-input{
+   width: 30%;
+  }
+  .b {
+    text-align: left;
+    /*background: yellow;*/
+    float: right;
+  }
   .aaa  .el-carousel__container {
     height: 240px;
     margin: 0;
